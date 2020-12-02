c0b2f76d412f4427fa3d5f263cd150effd1933b4
Balthamer <48745703+Balthamer@users.noreply.github.com>
2020-12-02 02:35:40 -0500

    Update random_sql_data.sh

--- random_sql_data.sh
+++ random_sql_data.sh
@@ -4,7 +4,7 @@ fname_array=(James  Mary  John  Patricia  Robert  Jennifer  Michael  Linda  Will
 declare -a lname_array
 lname_array=(Smith  Johnson  Williams  Brown  Jones  Garcia  Miller  Davis  Rodriguez  Martinez  Hernandez  Lopez  Gonzales  Wilson  Anderson  Thomas  Taylor  Moore  Jackson  Martin  Lee  Perez  Thompson  White  Harris  Sanchez  Clark  Ramirez  Lewis  Robinson  Walker  Young  Allen  King  Wright  Scott  Torres  Nguyen  Hill  Flores  Green  Adams  Nelson  Baker  Hall  Rivera  Campbell  Mitchell  Carter  Roberts  Gomez  Phillips  Evans  Turner  Diaz  Parker  Cruz  Edwards  Collins  Reyes  Stewart  Morris  Morales  Murphy  Cook  Rogers  Gutierrez  Ortiz  Morgan  Cooper  Peterson  Bailey  Reed  Kelly  Howard  Ramos  Kim  Cox  Ward  Richardson  Watson  Brooks  Chavez  Wood  James  Bennet  Gray  Mendoza  Ruiz  Hughes  Price  Alvarez  Castillo  Sanders  Patel  Myers  Long  Ross  Foster  Jimenez)
 declare -a location_array
-location_array=(Corporate Franklin Greenwood Columbus)
+location_array=(Corporate South East West)
 for i in {006..100}; do 
     temp=`echo $RANDOM`
     year=$(echo $temp % 50 | bc)
