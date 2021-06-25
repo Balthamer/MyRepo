bb86ca488d551f19f2f1c518d7391f011021641b
Balthamer <48745703+Balthamer@users.noreply.github.com>
2021-06-25 17:08:16 -0400

    Update startup.sh

--- Arch/startup.sh
+++ Arch/startup.sh
@@ -4,7 +4,7 @@
 echo '[multilib]' >> /etc/pacman.conf
 echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
 
-pacman -Sy vi vim networkmanager man-db man-pages grub zsh zsh-autosuggestions zsh-completions tmux gdb ghex bc htop wget usbutils git tree pacman-contrib lsof dmidecode dialog notepadqq zip unzip unrar p7zip traceroute bind-tools whois nmap gnu-netcat openssh openvpn cronie xdg-user-dirs haveged alsa-utils alsa-plugins pulseaudio pulseaudio-alsa gsfonts ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-hanazono ttf-baekmuk screengrab sxiv zathura texlive-most biber alacritty figlet ranger python-pywal mpd ncmpcpp cherrytree awesome xorg-xinit rofi
+pacman -Sy vi vim networkmanager man-db man-pages grub zsh zsh-autosuggestions zsh-completions tmux gdb ghex bc htop wget usbutils git tree pacman-contrib lsof dmidecode dialog notepadqq zip unzip unrar p7zip traceroute bind-tools whois nmap gnu-netcat openssh openvpn cronie xdg-user-dirs haveged alsa-utils alsa-plugins pulseaudio pulseaudio-alsa gsfonts ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-hanazono ttf-baekmuk screengrab sxiv zathura texlive-most biber alacritty figlet ranger python-pywal mpd ncmpcpp cherrytree awesome xorg-xinit rofi --noconfirm
 
 grub-install --efi-directory=/boot --target=x86_64-efi
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g' /etc/default/grub
