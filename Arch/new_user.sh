e4e084e622931d56470fa1b49dfa6738b9eb9ab2
balthamer <balthamer@grimoire.localdomain>
2021-06-26 17:20:20 -0400

    More updates

--- Arch/new_user.sh
+++ Arch/new_user.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-useradd -m -s $(which zsh) -G wheel,audio,video,optical,storage $1
-passwd $1
+echo "What is the new user name?"
+read user
 
-curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/zshrc > /home/$1/.zshrc
-curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/xinitrc > /home/$1/.xinitrc
-curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/vimrc > /home/$1/.vimrc
-curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/tmux.conf > /home/$1/.tmux.conf
-mkdir /home/$1/.config/alacritty
-curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/config/alacritty/alacritty.yml > /home/$1/.config/alacritty/alacritty.yml
-mkdir /home/$1/.config/awesome
-curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/config/awesome/rc.lua > /home/$1/.config/awesome/rc.lua
-mkdir /home$1/.config/mpd
-curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/config/mpd/mpd.conf > /home/$1/.config/mpd/mpd.conf
+useradd -m -s $(which zsh) -G wheel,audio,video,optical,storage $user
 
-chown -R $1:$1 /home/$1
+curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/zshrc > /home/$user/.zshrc
+curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/xinitrc > /home/$user/.xinitrc
+curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/vimrc > /home/$user/.vimrc
+curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/tmux.conf > /home/$user/.tmux.conf
+mkdir -p /home/$user/.config/alacritty
+curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/config/alacritty/alacritty.yml > /home/$user/.config/alacritty/alacritty.yml
+mkdir /home/$user/.config/awesome
+curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/config/awesome/rc.lua > /home/$user/.config/awesome/rc.lua
+mkdir /home/$user/.config/mpd
+curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/config/mpd/mpd.conf > /home/$user/.config/mpd/mpd.conf
+mkdir /home/$user/.config/ranger
+curl https://raw.githubusercontent.com/Balthamer/MyRepo/main/Arch/Dots/config/ranger/rc.conf > /home/$user/.config/ranger/rc.conf
+
+chown -R $user:$user /home/$user
+
+passwd $user
